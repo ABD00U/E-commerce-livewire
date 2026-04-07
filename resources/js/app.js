3443fc1ef2661333c7956e02f564f1c730b52405
Abdelrahman Elsayed <152273109+ABD00U@users.noreply.github.com>
2026-04-07 23:14:22 +0200

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,1 @@
 import "./bootstrap";
-import Alpine from "alpinejs";
-
-window.Alpine = Alpine;
-Alpine.store("auth", {
-    isAuthenticated: false,
-    showAuthModal: false,
-
-    redirect(url) {
-        if (this.isAuthenticated) {
-            window.location.href = url;
-        } else {
-            this.showAuthModal = true;
-        }
-    },
-});
